9730a4acb8534f5f256aa47fe1a605f67ed0d1cf
gustavo-it <gustavo_castrosilva@yahoo.com>
2025-07-18 16:42:00 -0300

    Tipos de dados primitivos

---
+++ 01-javascript-basico/09-primitive-data.js
@@ -0,0 +1,18 @@
+/**
+ * Exemplos de tipos de dados primitivos em JavaScript.
+ *
+ * @constant {string} firstName - Nome da pessoa.
+ * @constant {number} num - Número inteiro.
+ * @constant {number} num2 - Número decimal (ponto flutuante).
+ * @type {undefined} age - Variável sem valor atribuído (undefined).
+ * @type {null} lastName - Variável explicitamente definida como nula (null).
+ * @constant {boolean} isTrue - Valor booleano verdadeiro.
+ * @constant {boolean} isFalse - Valor booleano falso.
+ */
+const firstName = 'Maria';
+const num = 100;
+const num2 = 1.7;
+let age;
+let lastName = null;
+const isTrue = true;
+const isFalse = false;
