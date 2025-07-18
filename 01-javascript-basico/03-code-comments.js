3e2dee47de6635b7d65e383a4b4ebec78eb9f427
gustavo-it <gustavo_castrosilva@yahoo.com>
2025-07-18 10:20:10 -0300

    Code comments

---
+++ 01-javascript-basico/03-code-comments.js
@@ -0,0 +1,9 @@
+// Comentário de linha única
+
+console.log('Hello World!'); // Trecho ignorado
+
+/* Comentário de bloco
+   Este é um comentário de bloco que pode ocupar várias linhas.
+   Ele é útil para descrever seções maiores de código ou para desativar temporariamente
+   partes do código.    
+*/
