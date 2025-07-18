3f2e2cdb3295ea65a8403c5ac2da73831b7d955d
gustavo-it <gustavo_castrosilva@yahoo.com>
2025-07-18 10:50:16 -0300

    Browser vs Node (HTML + JavaScript)

---
+++ 01-javascript-basico/04-index.js
@@ -0,0 +1,8 @@
+/**
+ * Logs a message to the browser console and displays an alert dialog.
+ *
+ * The first line outputs a message to the browser's console for debugging or informational purposes.
+ * The second line shows an alert dialog box with a message to the user.
+ */
+console.log('Trecho exibido no navegador');
+alert('Alerta exibido no navegador');
