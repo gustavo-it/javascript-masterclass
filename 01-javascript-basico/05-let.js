3257ca5db6abfcae3e206e6ba46698be1c6f4648
gustavo-it <gustavo_castrosilva@yahoo.com>
2025-07-18 11:18:44 -0300

    Variables with let

---
+++ 01-javascript-basico/05-let.js
@@ -0,0 +1,17 @@
+/**
+ * Declares personal information variables and logs a formatted message to the console.
+ *
+ * Variables:
+ * @let {string} firstName - The first name of the person.
+ * @let {string} lastName - The last name of the person.
+ * @let {number} age - The age of the person.
+ *
+ * Logs:
+ * Prints a message to the console in the format:
+ * "Meu nome é {firstName} {lastName}. Tenho {age} anos."
+ */
+let firstName = 'Maria';
+let lastName = 'Silva';
+let age = 30;
+
+console.log(`Meu nome é ${firstName} ${lastName}. Tenho ${age} anos.`);
