b48646bd2c8d0ae6f8e766cda42583c97ce76563
gustavo-it <gustavo_castrosilva@yahoo.com>
2025-07-18 16:20:41 -0300

    Let vs Var - Primeira diferença

---
+++ 01-javascript-basico/08-var-let.js
@@ -0,0 +1,7 @@
+/**
+ * Declara e redeclara a variável 'firstName' usando 'var'.
+ * A primeira declaração atribui 'Maria' à variável.
+ * A segunda declaração sobrescreve o valor para 'Hello'.
+ */
+var firstName = 'Maria';
+var firstName = 'Hello';
