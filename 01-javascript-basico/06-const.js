760c457c8e36a04efb33c811caf91c705e6af93c
gustavo-it <gustavo_castrosilva@yahoo.com>
2025-07-18 13:46:39 -0300

    Constants with const

---
+++ 01-javascript-basico/06-const.js
@@ -0,0 +1,22 @@
+/**
+ * Declares personal information constants and logs a greeting message to the console.
+ *
+ * @constant {string} firstName - The first name of the person.
+ * @constant {string} lastName - The last name of the person.
+ * @constant {number} age - The age of the person.
+ *
+ * Logs a message in the format:
+ * "Hello, my name is {firstName} {lastName} and I am {age} years old."
+ */
+const firstName = 'Maria';
+const lastName = 'Silva';
+const age = 30;
+console.log(
+  'Hello, my name is ' +
+    firstName +
+    ' ' +
+    lastName +
+    ' and I am ' +
+    age +
+    ' years old.',
+);
