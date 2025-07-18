8f180963601d80b74455f848d0a554848f3d4612
gustavo-it <gustavo_castrosilva@yahoo.com>
2025-07-18 10:08:31 -0300

    Exercise - 01

---
+++ 01-javascript-basico/02-exercicio.js
@@ -0,0 +1,9 @@
+/**
+ * Logs a message to the console stating the user's name and the time they are learning JavaScript.
+ * The message includes the name 'Maria' and the time '10 da manhã'.
+ */
+console.log(
+  'Meu nome é \'Maria\'. Estou aprendendo JavaScript às',
+  10,
+  'da manhã'
+);
