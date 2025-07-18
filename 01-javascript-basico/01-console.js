8c68e37fcd443d7bab6d84ffa03b50c07db3d36e
gustavo-it <gustavo_castrosilva@yahoo.com>
2025-07-17 22:46:56 -0300

    Code documentation

--- 01-javascript-basico/01-console.js
+++ 01-javascript-basico/01-console.js
@@ -1,3 +1,9 @@
+/**
+ * Logs the string "Maria" to the console using different types of string literals:
+ * - Double quotes
+ * - Single quotes
+ * - Template literals
+ */
 console.log("Maria");
 console.log('Maria');
 console.log(`Maria`);
