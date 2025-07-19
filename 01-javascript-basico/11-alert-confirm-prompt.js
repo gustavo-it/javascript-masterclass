3b45fcb4703b318d49352ef5de298671279570e7
gustavo-it <gustavo_castrosilva@yahoo.com>
2025-07-19 15:18:27 -0300

    Alert, confirm e prompt (Navegador)

---
+++ 01-javascript-basico/11-alert-confirm-prompt.js
@@ -0,0 +1,7 @@
+/**
+ * Solicita ao usuário dois números por meio de prompts, calcula a soma e exibe o resultado em um alerta.
+ */
+const number1 = Number(prompt('Enter the first number:'));
+const number2 = Number(prompt('Enter the second number:'));
+
+alert(`The sum of ${number1} and ${number2} is ${number1 + number2}.`);
