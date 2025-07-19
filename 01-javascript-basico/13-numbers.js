ddf960ce0825120c12253a0f6ebe7abc7576c5de
gustavo-it <gustavo_castrosilva@yahoo.com>
2025-07-19 17:13:26 -0300

    Mais sobre numbers

---
+++ 01-javascript-basico/13-numbers.js
@@ -0,0 +1,14 @@
+let num1 = 10;
+num1 = num1.toString();
+console.log(num1, typeof num1);
+
+let num2 = 25.5231145;
+let num3 = 100;
+console.log(num2.toFixed(2));
+
+console.log(Number.isInteger(num3));
+console.log(Number.isInteger(num2));
+console.log(Number.isNaN('texto' * num1));
+
+const calc = (0.7 * 100 + 0.1 * 100) / 100;
+console.log(calc);
