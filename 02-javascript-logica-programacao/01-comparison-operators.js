2d913336da4fbfa02fae2a5d5338684f7df3c276
gustavo-it <gustavo_castrosilva@yahoo.com>
2025-07-22 10:29:02 -0300

    Operadores de comparação

---
+++ 02-javascript-logica-programacao/01-comparison-operators.js
@@ -0,0 +1,21 @@
+/* 
+Operadores de comparação
+
+> - Maior que
+>= - Maior ou igual a
+< - Menor que
+<= - Menor ou igual a
+== - igual a
+=== - Igual a (checa o valor e tipo)
+!= - Difente a
+!== - Diferente a (checa o valor e tipo)
+
+*/
+console.log(5 > 2); // true
+console.log(5 >= 5); // true
+console.log(5 < 2); // false
+console.log(5 <= 5); // true
+console.log(5 == '5'); // true
+console.log(5 === '5'); // false
+console.log(5 != '5'); // false
+console.log(5 !== '5'); // true
